b577bec0a6d9880a16eab750f09763121497d7ad
Abdulmajeed Jaafer <abdulmajeedjaafer76@gmail.com>
2021-06-01 07:07:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,13 +11,12 @@ function postData( name, email ) {
         email
       } )
     } )
-    .then( function ( response ) {
-      return response.json()
-    } )
-    .then( function ( object ) {
+    .then(  ( response ) => response.json()
+     )
+    .then( object => {
       document.body.innerHTML = object[ "id" ]
     } )
-    .catch( function ( error ) {
+    .catch( error => {
       document.body.innerHTML = error.message
     } )
 }
